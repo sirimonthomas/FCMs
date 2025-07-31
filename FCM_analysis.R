a66043e9bbd5d884f801f17437fd7b67e8ef974f
sirimonthomas <123623804+sirimonthomas@users.noreply.github.com>
2025-07-31 18:53:41 +0300

    added functions

--- FCM_analysis.R
+++ FCM_analysis.R
@@ -7,13 +7,18 @@ pacman::p_load(
   here,
   igraph,
   ggnetwork,
-  readxl
+  readxl,
+  FCMapper
 )
+
+source(here('functions.R'))
 
 #load FCMs
 fcm <- list()
 fcm.graph <- list()
 fcm.adj.matrix <- list()
+concepts.all <- character()
+concepts.cumulative <- numeric()
 for (file in list.files(here('input'), pattern = '.xlsx', full.names = F)) {
   obj.name <- paste0('fcm_',str_sub(file, start = 1, end = 2))
   #import edge list
@@ -30,12 +35,29 @@ for (file in list.files(here('input'), pattern = '.xlsx', full.names = F)) {
   
   write.csv(as.matrix(fcm.adj.matrix[[obj.name]]), here('output',paste0(obj.name,'_adjacency_matrix.csv')),
             row.names = T)
+  
+  #for calculating cumulative number of concepts
+  concepts.all <- c(concepts.all,colnames(fcm.adj.matrix[[obj.name]])) %>% unique()
+  #concepts.all <- c(concepts.all,V(fcm.graph[[obj.name]])$name) %>% unique()
+  
+  concepts.cumulative[obj.name] <- length(concepts.all)
+  names(concepts.cumulative) <- 1:length(concepts.cumulative)
 }
 
-#plot
-#basic plot
-plot(fcm.graph$fcm_02, edge.label = E(fcm.graph$fcm_02)$weight)
+#plot cumulative concepts to check for saturation (leveling off)
+plot(1:length(fcm),concepts.cumulative,
+     xlab = 'FCM number',
+     ylab = 'Cumulative Number of Unique Concepts')
+
 
 #aggregate all FCMs
-fcm$all <- 
+combined.adj.mat <- average_adjacency_matrices(fcm.adj.matrix)
+
+#create igraph
+combined.fcm <- graph_from_adjacency_matrix(combine)
+
+#basic plot
+plot(combined_fcm, edge.label = E(combined_fcm)$weight)
+
+  
   
